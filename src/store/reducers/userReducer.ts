dff2ac75a63e58c3f82fe689cfec1d9ce3ba4fa2
Chupriarti <wallx13@gmail.com>
2021-11-28 09:13:22 +0300

    refactoring

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,30 +1,5 @@
-export enum UserActionTypes {
-    FETCH_USERS = "FETCH_USERS",
-    FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS",
-    FETCH_USERS_ERROR = "FETCH_USERS_ERROR"
-}
+import { IUserState, UserAction, UserActionTypes } from "../../types/user"
 
-interface IUserState {
-    users: any[];
-    loading: boolean;
-    error: any | string;
-}
-
-interface IFetchUsersAction {
-    type: UserActionTypes.FETCH_USERS;
-}
-
-interface IFetchUsersSuccessAction {
-    type: UserActionTypes.FETCH_USERS_SUCCESS;
-    payload: any[];
-}
-
-interface IFetchUsersErrorAction {
-    type: UserActionTypes.FETCH_USERS_ERROR;
-    payload: string;
-}
-
-type UserAction = IFetchUsersAction | IFetchUsersSuccessAction | IFetchUsersErrorAction;
 
 const initialStte: IUserState = {
     users: [],
