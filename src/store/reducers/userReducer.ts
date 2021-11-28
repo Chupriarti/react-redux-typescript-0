4e174c6ab59b0c99eedaef5d0925ae491c29c6b7
Chupriarti <wallx13@gmail.com>
2021-11-28 08:58:46 +0300

    prepared userReducer

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -28,3 +28,22 @@ interface IUserAction {
     type: string;
     payload?: any;
 }
+
+const initialStte: IUserState = {
+    users: [],
+    loading: false,
+    error: null
+}
+
+export const userReducer = (state = initialStte, action: IUserAction): IUserState => {
+    switch (action.type) {
+        case UserActionTypes.FETCH_USERS:
+            return {loading: true, error: null, users: [] }
+        case UserActionTypes.FETCH_USERS_SUCCESS:
+            return {loading: false, error: null, users: action.payload }
+        case UserActionTypes.FETCH_USERS_ERROR:
+            return {loading: false, error: null, users: [] }
+        default:
+            return state;
+    }
+}
