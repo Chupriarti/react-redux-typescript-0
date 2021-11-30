a7c059ac3b315a936662d082e332d46bc5207fa3
Chupriarti <wallx13@gmail.com>
2021-11-30 21:16:49 +0300

    added slow loading emulation

--- src/store/action-creators/user.ts
+++ src/store/action-creators/user.ts
@@ -7,7 +7,10 @@ export const fetchUsers = () => {
         try {
             dispatch({type: UserActionTypes.FETCH_USERS})
             const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-            dispatch({type: UserActionTypes.FETCH_USERS_SUCCESS, payload: response.data})
+            setTimeout(() => {
+                dispatch({type: UserActionTypes.FETCH_USERS_SUCCESS, payload: response.data})
+            }, 1000);
+            
         } catch (e) {
             dispatch({type: UserActionTypes.FETCH_USERS_ERROR, payload: "Error during users loading"})
         }
