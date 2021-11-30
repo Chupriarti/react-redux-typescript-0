af6789314cbf129f5f9a82cfdf6e37d043e14db2
Chupriarti <wallx13@gmail.com>
2021-11-30 20:59:11 +0300

    finished action creator

--- src/store/action-creators/user.ts
+++ src/store/action-creators/user.ts
@@ -1,3 +1,4 @@
+import axios from "axios"
 import { Dispatch } from "redux"
 import { UserActionTypes } from "../../types/user"
 
@@ -5,6 +6,8 @@ export const fetchUsers = () => {
     return async (dispatch: Dispatch) => {
         try {
             dispatch({type: UserActionTypes.FETCH_USERS})
+            const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+            dispatch({type: UserActionTypes.FETCH_USERS_SUCCESS, payload: response.data})
         } catch (e) {
             dispatch({type: UserActionTypes.FETCH_USERS_ERROR, payload: "Error during users loading"})
         }
