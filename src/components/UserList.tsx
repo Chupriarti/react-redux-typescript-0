2a5d9873675f54747064a8b388a196ccb26301f9
Chupriarti <wallx13@gmail.com>
2021-11-30 21:01:45 +0300

    added fetchUsers usage to UserList

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { fetchUsers } from '../store/action-creators/user';
 
 const UserList: React.FC = () => {
     const {users, error, loading} = useTypedSelector(state => state.user);
+    const dispatch  = useDispatch();
+
+    React.useEffect(() => {
+        dispatch(fetchUsers);
+    }, []);
+
+    
 
     return (
         <div>
